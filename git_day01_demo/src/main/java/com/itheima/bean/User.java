2e1d9f881fc263311926c88ef4c4d9592c48065a
fengyuming <454943554@qq.com>
2020-02-10 19:54:58 +0800

    修改了User

--- git_day01_demo/src/main/java/com/itheima/bean/User.java
+++ git_day01_demo/src/main/java/com/itheima/bean/User.java
@@ -3,36 +3,7 @@ package com.itheima.bean;
 public class User {
     private Integer id;
     private String name;
+    private String password;
 
-    public User() {
-    }
 
-    public User(Integer id, String name) {
-        this.id = id;
-        this.name = name;
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    @Override
-    public String toString() {
-        return "User{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                '}';
-    }
 }
